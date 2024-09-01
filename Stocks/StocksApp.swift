8739f639ce7a74efc52ad29916330d844a61cf1c
Dastan Tashimbetov <arabesku@gmail.com>
2024-09-01 12:34:54 -0700

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Dastan Tashimbetov on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
